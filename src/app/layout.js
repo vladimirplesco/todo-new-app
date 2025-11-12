d5a13805996ebd93e21d0354ec1ee001faf8af56
Vladimir Plesco <pleshco@deeplace.md>
2025-11-12 15:04:36 +0200

    p-4

--- src/app/layout.js
+++ src/app/layout.js
@@ -13,7 +13,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
       <html lang='ru' className={manrope.className}>
-        <body>{children}</body>
+        <body className='p-4'>{children}</body>
       </html>
   ); 
 }
