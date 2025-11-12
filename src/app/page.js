5633e644651b0826e60726a206f26262e02e783a
Vladimir Plesco <pleshco@deeplace.md>
2025-11-12 16:07:12 +0200

    List

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,22 @@
 
 export default function Home() {
   return (
-    <div className="flex gap-4">
-      <input  className="input input-primary" type="text" />
-      <button className="btn btn-primary">Кнопка</button>
+    <div>
+      <div className="flex gap-4">
+        <input  className="input input-primary" type="text" />
+        <button className="btn btn-primary">Кнопка</button>
+      </div>
+      
+      <div className="flex flex-col gap-4">
+      <div>Я</div>
+      <div>на</div>
+      <div>солнышке</div>
+      <div>лежу,</div>
+      <div>я</div>
+      <div>на</div>
+      <div>солнышко</div>
+      <div>гляжу!</div>
+      </div>
     </div>
   );
 }
