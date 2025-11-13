f2952b454b4342701fa1a5a5f221cd737f2d5561
Vladimir Plesco <pleshco@deeplace.md>
2025-11-13 10:46:36 +0200

    new key for mapping

--- src/app/page.js
+++ src/app/page.js
@@ -1,13 +1,14 @@
 
 export default function Home() {
-  const vers = ["Я на солнышке сижу", 
-    "Я на солнышко гляжу", 
-    "Всё сижу и сижу", 
-    "И на солнышко гляжу.", 
-    "Носорог-рог-рог идёт,", 
-    "Крокодил-дил-дил плывёт,", 
-    "Только я все сижу", 
-    "И на солнышко гляжу."
+  const vers = [
+    {id: 0,value:"Я на солнышке сижу"}, 
+    {id: 1,value:"Я на солнышко гляжу"}, 
+    {id: 2,value:"Всё сижу и сижу"}, 
+    {id: 3,value:"И на солнышко гляжу."}, 
+    {id: 4,value:"Носорог-рог-рог идёт,"}, 
+    {id: 5,value:"Крокодил-дил-дил плывёт,"}, 
+    {id: 6,value:"Только я все сижу"}, 
+    {id: 7,value:"И на солнышко гляжу."}
   ];
   return (
     <div>
@@ -16,7 +17,7 @@ export default function Home() {
         <button className="btn btn-primary">Кнопка</button>
       </div>
       <ul class="list bg-base-200 rounded-box gap-4">
-       {vers.map((string, index) => <li key={index}>{string}</li>)}
+       {vers.map((string) => <li key={string.id}>{string.value}</li>)}
       </ul>
     </div>
   );
