4a451a3d4922432811ba472da602037b2b7de51a
Vladimir Plesco <pleshco@deeplace.md>
2025-11-17 16:39:48 +0200

    Clear array

--- src/app/page.js
+++ src/app/page.js
@@ -4,16 +4,7 @@ import { useState } from "react";
 
 export default function Home() {
 
-  const [vers, setVers] = useState([
-    {id: "0", value:"Я на солнышке сижу"}, 
-    {id: "1", value:"Я на солнышко гляжу"}, 
-    {id: "2", value:"Всё сижу и сижу"}, 
-    {id: "3", value:"И на солнышко гляжу."}, 
-    {id: "4", value:"Носорог-рог-рог идёт,"}, 
-    {id: "5", value:"Крокодил-дил-дил плывёт,"}, 
-    {id: "6", value:"Только я все сижу"}, 
-    {id: "7", value:"И на солнышко гляжу."},
-  ]);
+  const [vers, setVers] = useState([]);
 
   const [inputValue, setInputValue] = useState("");
 
