97e19387d35ae0518eeb417f2f77edab8eae2a47
Vladimir Plesco <pleshco@deeplace.md>
2025-11-17 10:14:40 +0200

    Corrected styles

--- src/app/page.js
+++ src/app/page.js
@@ -10,13 +10,13 @@ export default function Home() {
     {id: "7", value:"И на солнышко гляжу."},
   ];
   return (
-    <div className="max-w-xl mx-auto p-4">
-      <div className="flex flex-wrap gap-4 w-full justify-center">
+    <div className="max-w-xl mx-auto">
+      <div className="flex flex-wrap gap-4 w-full justify-center mb-6">
         <input  className="input input-primary grow pe-1" type="text" placeholder="Введите текст"/>
-        <button className="btn btn-primary">Кнопка</button>
+        <button className="btn btn-primary grow">Кнопка</button>
       </div>
-      <ul className="list bg-base-200 rounded-box flex flex-col gap-4">
-        {vers.map((item) => <li className="border-base-content/10 p-4 border w-full" key={item.id}>{item.value}</li>)}
+      <ul className="list bg-base-200 rounded-box flex flex-col">
+        {vers.map((item) => <li className="border-b border-base-content/10 last:border-b-0 p-2 w-full" key={item.id}>{item.value}</li>)}
       </ul>
     </div>
   );
