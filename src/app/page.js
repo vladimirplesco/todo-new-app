3ea7ba619d8672f158396164e2ad1389f54f6bba
Vladimir Plesco <pleshco@deeplace.md>
2025-11-17 16:28:44 +0200

    Ability to add item

--- src/app/page.js
+++ src/app/page.js
@@ -1,5 +1,10 @@
+'use client';
+
+import { useState } from "react";
+
 export default function Home() {
-  const vers = [
+
+  const [vers, setVers] = useState([
     {id: "0", value:"Я на солнышке сижу"}, 
     {id: "1", value:"Я на солнышко гляжу"}, 
     {id: "2", value:"Всё сижу и сижу"}, 
@@ -8,12 +13,31 @@ export default function Home() {
     {id: "5", value:"Крокодил-дил-дил плывёт,"}, 
     {id: "6", value:"Только я все сижу"}, 
     {id: "7", value:"И на солнышко гляжу."},
-  ];
+  ]);
+
+  const [inputValue, setInputValue] = useState("");
+
+  function addItem() {
+      const newItem = {
+        id: crypto.randomUUID(),
+        value: inputValue.trim(),
+      };
+
+      if (newItem.value.length > 0) {
+        setVers([...vers, newItem]);
+        setInputValue("");
+      }
+  }
+
+  function handleTextChange(e) {
+    setInputValue(e.target.value);
+  }
+
   return (
     <div className="max-w-xl mx-auto">
       <div className="flex flex-wrap gap-4 w-full justify-center mb-6">
-        <input  className="input input-primary grow" type="text" placeholder="Что нужно сделать?"/>
-        <button className="btn btn-primary grow">Добавить</button>
+        <input  className="input input-primary grow" type="text" placeholder="Что нужно сделать?" value={inputValue} onChange={function(event) { setInputValue(event.target.value) }}/>
+        <button className="btn btn-primary grow" onClick={addItem}>Добавить</button>
       </div>
       <ul className="list bg-base-200 rounded-box flex flex-col">
         {vers.map((item) => <li className="border-b border-base-content/10 last:border-b-0 px-2 py-4 w-full" key={item.id}>{item.value}</li>)}
