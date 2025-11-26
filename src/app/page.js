37bfd1fe5a3dddc51ee344b970fe4a019dbb49d9
Vladimir Plesco <pleshco@deeplace.md>
2025-11-26 15:43:44 +0200

    Corrected form component

--- src/app/page.js
+++ src/app/page.js
@@ -1,28 +1,18 @@
 'use client';
 
-import { useState, useRef } from "react";
+import { useState } from "react";
 import { Title } from "../ui/title/title";
 import { Form } from "../ui/form/form";
 import { List } from "../ui/list/list";
 
 export default function Home() {
   const [vers, setVers] = useState([]);
-  const inputRef = useRef(null);
-  function addItem() {
-
-      const newItem = {
-        id: crypto.randomUUID(),
-        value: inputRef.current.value.trim(),
-      };
-
-      setVers([...vers, newItem]);
-      inputRef.current.focus();
-  }
+  
 return (
     <div className="max-w-xl mx-auto">
       <div className="space-y-6">
         <Title />
-        <Form ref={ inputRef } addItem={ addItem }/>
+        <Form vers={ vers } setVers={ setVers }/>
         <List vers={ vers }/>
       </div>
     </div>
--- src/ui/form/form.js
+++ src/ui/form/form.js
@@ -1,10 +1,23 @@
 import React, { forwardRef } from 'react';
+import { useRef } from "react";
+  
 const Form = forwardRef((props, ref) => {
+  const inputRef = useRef(null);
+  function addItem() {
+  
+      const newItem = {
+        id: crypto.randomUUID(),
+        value: inputRef.current.value.trim(),
+      };
+
+      props.setVers([...props.vers, newItem]);
+      inputRef.current.focus();
+  }
   return (
     <div>
-    <form action={ props.addItem }>
+    <form action={ addItem }>
         <div className="flex flex-wrap gap-4 w-full justify-center mb-6">
-          <input ref={ ref } className="input input-primary grow" type="text" required placeholder="Что нужно сделать?" />
+          <input ref={ inputRef } className="input input-primary grow" type="text" required placeholder="Что нужно сделать?" />
           <button className="btn btn-primary grow" type='submit'>Добавить</button>
         </div>
       </form>
