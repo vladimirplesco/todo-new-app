697002333c24567bbe3e91d377e2e1c21f135427
Vladimir Plesco <pleshco@deeplace.md>
2025-12-05 16:27:02 +0200

    Improved cod

--- src/ui/list/list.js
+++ src/ui/list/list.js
@@ -1,15 +1,17 @@
 export function List( {vers, setVers} ) {
+  
+  function toggleCompleted(itemId) {
+    const newVers = vers.map(item => {
+      if (item.id === itemId) {
+        return { ...item, completed: !item.completed };
+      }
+      return item;
+    });
+    setVers(newVers);
+  }
   return (
     <ul className="list bg-base-200 rounded-box">
-      {vers.map((item) => <li className="border-b border-base-content/10 last:border-b-0 p-4 flex justify-between items-center" key={item.id}>{item.value}<input type="checkbox" className="checkbox checkbox-primary" checked={item.completed} onChange={() => {
-        const newVers = vers.map(v => {
-          if (v.id === item.id) {
-            return { ...v, completed: !v.completed };
-          }
-          return v;
-        });
-        setVers(newVers);
-      }} /></li>)}
+      {vers.map((item) => <li className="border-b border-base-content/10 last:border-b-0 p-4 flex justify-between items-center" key={item.id}>{item.value}<input type="checkbox" className="checkbox checkbox-primary" checked={item.completed} onChange={() => toggleCompleted(item.id)} /></li>)}
     </ul>
   );
 }
